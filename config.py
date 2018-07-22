c8ff86f2d40ee8f599b9f1a325635ca07e146065
Tree-lcf <lincf97@163.com>
2018-07-22 22:38:23 +0800

    Initial commit

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+SECRET_KEY = os.urandom(24)
+DEBUG = True
+
+HOSTNAME = '127.0.0.1'
+PORT = '3306'
+DATABASE = 'app'
+USERNAME = 'root'
+PASSWORD = 'root'
+DB_URI = 'mysql+mysqldb://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
+SQLALCHEMY_DATABASE_URI = DB_URI
---
+++ models.py
@@ -0,0 +1,10 @@
+import os
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Flask
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
